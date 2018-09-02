d8f0debca20b7fbf5fcf627bba54970cfceaa40d
AsheHyko <zyad1239@gmail.com>
2018-09-02 14:56:31 +0300

    new

--- index.js
+++ index.js
@@ -1,8 +1,49 @@
-var RedditRadio = require("./RedditRadio");
+require.extensions['.txt'] = function (module, filename) { module.exports = require("fs").readFileSync(filename, 'utf8'); };
 
-var bot = new RedditRadio();
-bot.start();
+const { Collection } = require("discord.js");
 
-process.on("SIGINT", function() {
-	bot.stop();
-});
+const build = require("./build");
+console.log(build);
+const config = require(`./configs/${build}`);
+
+const SHARD_COUNT   = config.shards;
+const CLIENT_TOKEN  = config.token;
+
+const Shard = require("./structures/Shard");
+
+class ShardingMaster extends Collection {
+    constructor() {
+        super();
+        
+        this.stats = [];
+
+        for (let s = 0; s < SHARD_COUNT; s++) {
+            setTimeout(() => this.set(s, new Shard(this, s, SHARD_COUNT, CLIENT_TOKEN, build)), (9000 * s));
+        }
+    }
+
+    broadcast(type, data) {
+        this.forEach(shard => {
+            shard.send({
+                type,
+                data
+            });
+        });
+    }
+
+    updateStats(shard, data) {
+        Object.keys(data).map(key => this.get(shard).stats[key] = data[key]);
+
+        const newData = {};
+        
+        this.forEach(shard => {
+            Object.keys(shard.stats).forEach(key => {
+                newData[key] ? newData[key] += shard.stats[key] : newData[key] = shard.stats[key];
+            });
+        });
+
+        this.broadcast("stats", newData);
+    }
+}
+
+new ShardingMaster();
